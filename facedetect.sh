22b9cf8601402e56bddd5247e1b87efe9e18304e
Mikhail Samoylov <mikhail.samoylov@timebook.ru>
2015-08-05 18:17:24 +0300

    fixed build script, add gitignore

--- facedetect.sh
+++ facedetect.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cd ../binary
-cmake ../source/$1
-make
+
+function build {
+    build_dir=$1_build
+    mkdir -p $build_dir
+    cd $build_dir
+    cmake ../$1
+    make
+    cd ..
+}
+
+build facecut
+build facedetect
